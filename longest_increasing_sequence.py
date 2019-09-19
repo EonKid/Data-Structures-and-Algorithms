bd25cd9df2d00ab03c5d21e7daa5ae6111054a71
Narayan Singh, Dhruv(dn5961) <dn5961@intl.att.com>
2019-09-19 13:34:22 +0530

    solved lis

--- longest_increasing_sequence.py
+++ longest_increasing_sequence.py
@@ -8,11 +8,19 @@ Longest increase in subsequence problem:
 def lis(A: list):
     n = len(A)
     L = [1] * n
+    T = []
     for i in range(0, n):
+        s_pairs = set()
         for j in range(i):
             if A[j] < A[i] and L[i] < L[j] + 1:
+                s_pairs.add(A[i])
                 L[i] = L[j] + 1
+                s_pairs.add(A[j])
+        if len(s_pairs):
+            T.append(s_pairs)
+
     print(max(L))
+    print(T)
 
 
 s = [7, 2, 1, 3, 8, 4, 9, 1, 2, 6, 5, 9, 3]
