6b84ee55dfd88f0f44feb1535ae1485191c2cd4b
EonKid <dhruvnarayan1023@gmail.com>
2019-11-09 15:01:45 +0530

    Leetcode: strings

---
+++ leetcode/strings/first_unique_char_in_str.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/881/
+
+
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        T = {}
+        result = float("inf")
+        if len(s) == 0:
+            return -1
+        for i in range(0, len(s)):
+            c = s[i]
+            if c not in T:
+                data = [i, 1]
+                T[c] = data
+            else:
+                data = T[c]
+                data[0] = i
+                data[1] = data[1] + 1
+        is_min_found = False
+        for key in T:
+            data = T[key]
+            if data[1] == 1:
+                is_min_found = True
+                result = min(result, data[0])
+        if not is_min_found:
+            return -1
+        return result
+
+
+sol = Solution()
+print(sol.firstUniqChar("aadadaad"))
---
+++ leetcode/strings/valid_anagram.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+# https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/882/
+
+
+class Solution:
+
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): return False
+        Ts = {}
+        Tr = {}
+        for c in s:
+            if c not in Ts:
+                Ts[c] = 1
+            else:
+                Ts[c] = Ts[c] + 1
+        for c in t:
+            if c not in Tr:
+                Tr[c] = 1
+            else:
+                Tr[c] = Tr[c] + 1
+
+        for key in Ts:
+            if key in Tr:
+                if Ts[key] != Tr[key]:
+                    return False
+            else:
+                return False
+        return True
+
+
+sol = Solution()
+print(sol.isAnagram("aacc" ,"ccac"))
