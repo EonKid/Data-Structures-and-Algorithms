3eefd88a680525afd1e3cdba0694e2a6fbccbf5b
EonKid <dhruvnarayan1023@gmail.com>
2019-12-21 13:37:48 +0530

    Alogrithmis Toolbox: Coin change

--- algorithms_toolbox/week3_greedy_algorithms/1_money_change/change.py
+++ algorithms_toolbox/week3_greedy_algorithms/1_money_change/change.py
@@ -1,10 +1,16 @@
 # Uses python3
 import sys
 
+
 def get_change(m):
-    #write your code here
-    return m
+    coins = [10, 5, 1]
+    result = []
+    for coin in coins:
+        while m >= coin:
+            m -= coin
+            result.append(coin)
+    return len(result)
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
