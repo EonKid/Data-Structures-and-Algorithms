56b5d74ee25307ba0d0d0df080abffa6b72a4e4a
EonKid <dhruvnarayan1023@gmail.com>
2019-12-21 19:16:28 +0530

    Leetcode: coin change

---
+++ leetcode/array/322_coin_change.py
@@ -0,0 +1,15 @@
+# python3
+# https://leetcode.com/problems/coin-change/
+
+
+class Solution:
+    def coinChange(self, coins: [int], amount: int) -> int:
+        dp = [amount + 1] * (amount + 1)
+        dp[0] = 0  # base case
+        for coin in coins:
+            for x in range(coin, amount + 1):
+                dp[x] = min(dp[x], dp[x - coin] + 1)
+        if dp[amount] > amount:
+            return -1
+        return dp[amount]
+
