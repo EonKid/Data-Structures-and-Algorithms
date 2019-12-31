9a0ca976abcbb8170fe94ec7653de659cc1248ee
EonKid <dhruvnarayan1023@gmail.com>
2019-12-31 14:41:26 +0530

    Algorithmic Toolbox

--- algorithms_toolbox_problems/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ algorithms_toolbox_problems/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -2,9 +2,17 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m // 4
+    T = dict()
+    T[0] = 0
+    coins = [2, 4, 8]
+    for m in range(1, m+1):
+        T[m] = float('inf')
+        for coin in coins:
+            if m >= coin:
+                T[m] = min(T[m], T[m-coin] + 1)
+    return T[m]
 
 if __name__ == '__main__':
-    m = int(sys.stdin.read())
+    # m = int(sys.stdin.read())
+    m = int(input())
     print(get_change(m))
--- algorithms_toolbox_problems/week5_dynamic_programming1/3_edit_distance/edit_distance.py
+++ algorithms_toolbox_problems/week5_dynamic_programming1/3_edit_distance/edit_distance.py
@@ -1,7 +1,26 @@
 # Uses python3
+
+
 def edit_distance(s, t):
-    #write your code here
-    return 0
+    n = len(s)
+    m = len(t)
+    D = [[float('inf')] * (m+1) for _ in range(n+1)]
+    for i in range(0, n+1):
+        D[i][0] = i
+    for j in range(0, m+1):
+        D[0][j] = j
+
+    for i in range(1, n+1):
+        for j in range(1, m+1):
+            insertion = D[i][j-1] + 1
+            deletion = D[i-1][j] + 1
+            mismatch = D[i-1][j-1] + 1
+            matched = D[i-1][j-1]
+            if s[i-1] == t[j-1]:
+                D[i][j] = min(insertion, deletion, matched)
+            else:
+                D[i][j] = min(insertion, deletion, mismatch)
+    return D[n][m]
 
 if __name__ == "__main__":
     print(edit_distance(input(), input()))
