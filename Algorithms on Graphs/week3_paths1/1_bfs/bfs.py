da6da7e754a83c8bf08d7a4e52b5e8cc63decf31
EonKid <dhruvnarayan1023@gmail.com>
2020-01-16 12:34:07 +0530

    Algorithms on Graph: BFS

--- Algorithms on Graphs/week3_paths1/1_bfs/bfs.py
+++ Algorithms on Graphs/week3_paths1/1_bfs/bfs.py
@@ -1,11 +1,24 @@
-#Uses python3
+# Uses python3
 
 import sys
 import queue
 
+
 def distance(adj, s, t):
-    #write your code here
+    dist = [None] * len(adj)
+    q = queue.Queue()
+    q.put(s)
+    dist[s] = 0
+    while not q.empty():
+        v = q.get()
+        for u in adj[v]:
+            if dist[u] is None:
+                dist[u] = dist[v] + 1
+                q.put(u)
+                if u == t:
+                    return dist[u]
     return -1
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
@@ -19,3 +32,5 @@ if __name__ == '__main__':
         adj[b - 1].append(a - 1)
     s, t = data[2 * m] - 1, data[2 * m + 1] - 1
     print(distance(adj, s, t))
+    # 4 4 1 2 4 1 2 3 3 1 2 4
+    # 5 4 5 2 1 3 3 4 1 4 3 5
