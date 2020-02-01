05a9afc27b0d1abf3cd9d27f48711ff837eac409
EonKid <dhruvnarayan1023@gmail.com>
2020-02-01 15:04:53 +0530

    Leetcode: 42

--- leetcode/hard/42_trapping_rain_water.py
+++ leetcode/hard/42_trapping_rain_water.py
@@ -7,21 +7,29 @@ Output: 6
 
 class Solution:
     def trap(self, height: [int]) -> int:
-        if len(height) == 0:
-            return 0
+        if len(height) == 0: return 0
         water_trapped = 0
-        L = [0]*len(height)
-        R = [0]*len(height)
+        left = 0
+        left_max = 0
+        n = len(height)
+        right = n - 1
+        right_max = 0
 
-        L[0] = height[0]
-        for i in range(1, len(height)):
-            L[i] = max(height[i], L[i-1])
-        last = len(height) - 1
-        R[last] = height[last]
-        last -= 1
-        while last >= 0:
-            R[last] = max(height[last], R[last+1])
-            last -= 1
-        for i in range(0, len(height)):
-            water_trapped += min(L[i], R[i]) - height[i]
+        while left < right:
+            if height[left] < height[right]:
+                if height[left] > left_max:
+                    left_max = height[left]
+                else:
+                    water_trapped += left_max - height[left]
+                left += 1
+            else:
+                if height[right] > right_max:
+                    right_max = height[right]
+                else:
+                    water_trapped += right_max - height[right]
+                right -= 1
+
         return water_trapped
+
+sol = Solution()
+print(sol.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
