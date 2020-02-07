2b6600de0aacc208e2c4b553e8730edc008a0967
EonKid <dhruvnarayan1023@gmail.com>
2020-02-07 13:49:26 +0530

    Data structures: solved  week 1 problems

--- Data Structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ Data Structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -11,19 +11,30 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
+    last_index = 0
     for i, next in enumerate(text):
+        last_index = i + 1
         if next in "([{":
-            # Process opening bracket, write your code here
-            pass
-
+            opening_brackets_stack.append({"position":last_index, "char": next})
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+           if len(opening_brackets_stack) == 0:
+               opening_brackets_stack.append({"position":last_index, "char": next})
+               break
+           if are_matching(opening_brackets_stack[-1]["char"], next):
+               opening_brackets_stack.pop()
+           else:
+               opening_brackets_stack.append({"position": last_index, "char": next})
+               break
+
+    if len(opening_brackets_stack) == 0:
+        return "Success"
+    return opening_brackets_stack[-1]["position"]
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
+    print(mismatch)
     # Printing answer, write your code here
 
 
--- Data Structures/week1_basic_data_structures/2_tree_height/tree-height.py
+++ Data Structures/week1_basic_data_structures/2_tree_height/tree-height.py
@@ -9,17 +9,34 @@ class TreeHeight:
                 self.n = int(sys.stdin.readline())
                 self.parent = list(map(int, sys.stdin.readline().split()))
 
+                self.nodes = []
+                self.root = None
+
+                self.nodes = [[] for i in range(self.n)]
+
+                for child_index in range(self.n):
+                        parent_index = self.parent[child_index]
+                        if parent_index == -1:
+                                self.root = child_index
+                        else:
+                                self.nodes[parent_index] += [child_index]
+
         def compute_height(self):
-                # Replace this code with a faster implementation
-                maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
-                                height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+                queue = [];
+                height = 0
+                queue.append(self.root)
+
+                while True:
+                        node_count = len(queue)
+                        if node_count == 0:
+                                return height
+                        height += 1
+                        while node_count > 0:
+                                node = queue.pop(0)
+                                if self.nodes[node]:
+                                        for x in self.nodes[node]:
+                                                queue.append(x)
+                                node_count -= 1
 
 def main():
   tree = TreeHeight()
