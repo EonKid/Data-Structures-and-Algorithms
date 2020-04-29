15a00ef77b3d306adb129e3dd0005df78a1b565d
EonKid <dhruvnarayan1023@gmail.com>
2020-04-29 08:12:26 +0530

    Leetcode: 72, output alignment

--- leetcode/dp/72_editDistance.swift
+++ leetcode/dp/72_editDistance.swift
@@ -3,6 +3,8 @@
 
 class Solution {
 
+    var D = [[Int]]()
+
     func minDistance(_ word1: String, _ word2: String) -> Int {
         if word1.count == 0 && word2.count == 0{ return 0 }
         if word1.count > 0 && word2.count == 0{ return word1.count }
@@ -10,7 +12,7 @@ class Solution {
 
         let word1 = Array(word1)
         let word2 = Array(word2)
-        var D = Array.init(repeating: Array.init(repeating: Int.max, count: word2.count + 1), count: word1.count + 1)
+        D = Array.init(repeating: Array.init(repeating: Int.max, count: word2.count + 1), count: word1.count + 1)
         for i in 0 ..< word1.count + 1 {
                 D[i][0] = i
          }
@@ -33,7 +35,35 @@ class Solution {
 
             }
         }
+        for data in D{
+            print(data)
+        }
         return D[word1.count][word2.count]
+    }
+
+    func outputAlignment(_ i: Int, _ j: Int){
+
+        if i == 0 && j == 0{
+            print("\(i), \(j): \(D[i][j])")
+            return
+        }
+        if i > 0 && D[i][j] == D[i-1][j] + 1{
+            print("\(i), \(j): \(D[i][j])")
+            self.outputAlignment(i - 1, j)
+        }else if j > 0 && D[i][j] == D[i][j - 1] + 1{
+            print("\(i), \(j): \(D[i][j])")
+            self.outputAlignment(i, j - 1)
+        }else {
+            print("\(i), \(j): \(D[i][j])")
+            outputAlignment(i - 1, j - 1)
+        }
     }
 }
 
+
+let sol = Solution()
+let word1 = "editing"
+let word2 = "distance"
+print("edit distance:  \(sol.minDistance(word1, word2))")
+print("Print output alignment:")
+sol.outputAlignment(word1.count, word2.count)
