0916d2b406b30fb311faff3bb78860e0ec356f05
EonKid <dhruvnarayan1023@gmail.com>
2020-05-17 17:31:13 +0530

    Leetcode: 120

--- leetcode/dp/120_triangle.swift
+++ leetcode/dp/120_triangle.swift
@@ -1,17 +1,38 @@
 // https://leetcode.com/problems/triangle/
 
 class Solution {
+
     func minimumTotal(_ triangle: [[Int]]) -> Int {
-         if triangle.count == 0{
-             return 0
-         }
-         let n = triangle.count
-         var dp = triangle[n - 1]
-         for row in stride(from: n - 2, to: -1, by: -1){
-             for col in stride(from: 0, to: row + 1, by: 1){
-                 dp[col] = triangle[row][col] + min(dp[col], dp[col+1])
-             }
-         }
-         return dp[0]
+
+        func sol2() -> Int{
+          if triangle.count == 0{ return 0 }
+           var prevRow = triangle[0]
+		   for i in stride(from: 1, to: triangle.count, by: 1){
+				var currRow = triangle[i]
+				currRow[0] = currRow[0] + prevRow[0]
+                  for j in stride(from: 1, to: currRow.count - 1, by: 1){
+							currRow[j] = currRow[j] + min(prevRow[j-1], prevRow[j])
+				}
+                currRow[currRow.count - 1] = currRow[currRow.count - 1] + prevRow[prevRow.count - 1]
+				prevRow = currRow
+		  }
+           return prevRow.min() ?? 0
+        }
+
+        func sol1() -> Int{
+            let n = triangle.count
+            if n == 0{ return 0 }
+            var dp = triangle[n - 1]
+            for row in stride(from: triangle.count - 2, to: -1, by: -1){
+                for col in stride(from: 0, to: row + 1, by: 1){
+                    dp[col] = triangle[row][col] + min(dp[col], dp[col + 1])
+                }
+            }
+            return dp[0]
+        }
+        return sol1()
     }
+
 }
+
+
