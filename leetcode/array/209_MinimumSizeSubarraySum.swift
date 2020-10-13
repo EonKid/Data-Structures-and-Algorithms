4f41d59b79057c4fa92ab5a1da0b9b05db49a7da
EonKid <dhruvnarayan1023@gmail.com>
2020-10-13 12:13:09 +0530

    Sliding window: longest substring k distincts

--- leetcode/array/209_MinimumSizeSubarraySum.swift
+++ leetcode/array/209_MinimumSizeSubarraySum.swift
@@ -34,6 +34,45 @@ func findMaxSumSubarray(_ nums: [Int], _ k: Int) -> Int{
 
 }
 
+func longestSubstringKDistinct(_ s: String, _ k: Int) -> Int {
+
+    if s.count == 0 || s.count < k{
+        return -1
+    }
+
+    // s = "aabbcc" k = 2
+    // map = []
+    var map = [Character: Int]()
+    var maxLen = Int.min
+    var windowStart = 0
+    let s = Array(s)
+    for windowEnd in stride(from: 0, to: s.count, by: 1){
+        let rightChar = s[windowEnd]
+        if let charCount = map[rightChar]{
+            map[rightChar] = charCount + 1
+        }else{
+            map[rightChar] = 1
+        }
+
+        while map.count > k {
+            let leftChar = s[windowStart]
+            if let charCount = map[leftChar]{
+                map[leftChar] = charCount - 1
+            }
+            if let cCount = map[leftChar] {
+                if cCount == 0{
+                    map.removeValue(forKey: leftChar)
+                }
+            }
+            windowStart += 1
+        }
+        maxLen = max(maxLen, windowEnd - windowStart + 1)
+    }
+    return maxLen
+}
+
+print("Longest substring: \(longestSubstringKDistinct("aaabbb", 3))")
+
 
 
 print(findMaxSumSubarray([4, 2, 1, 7, 8, 1, 2, 8, 1, 0], 3))
