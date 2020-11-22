c1a0dbb948154f962f3c5c34b4016ecc9eef5176
EonKid <dhruvnarayan1023@gmail.com>
2020-11-22 20:02:08 +0530

    DS: binary search tree

--- coursera/data_structures_2020/week4_binary_search_trees/2_is_bst/is_bst.py
+++ coursera/data_structures_2020/week4_binary_search_trees/2_is_bst/is_bst.py
@@ -5,8 +5,40 @@ import sys, threading
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**25)  # new thread will get stack of such size
 
+
+class Node:
+
+  def __init__(self, key, left, right):
+    self.key = key
+    self.left = left
+    self.right = right
+
+def in_order_traversal(tree):
+  nodes = []
+  result = []
+  stack = []
+  current = 0
+  for node in tree:
+    nodes.append(Node(key=node[0], left= node[1], right= node[2]))
+
+  while stack or current != -1:
+    if current != -1:
+      root = nodes[current]
+      stack.append(root)
+      current = root.left
+    else:
+      root = stack.pop()
+      result.append(root.key)
+      current = root.right
+  return result
+
 def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
+  if len(tree) == 0:
+    return True
+  nodes = in_order_traversal(tree)
+  for i in range(1, len(nodes)):
+    if nodes[i] <= nodes[i - 1]:
+      return False
   return True
 
 
--- coursera/data_structures_2020/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
+++ coursera/data_structures_2020/week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
@@ -5,8 +5,27 @@ import sys, threading
 sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**25)  # new thread will get stack of such size
 
+
+class Node:
+
+  def __init__(self, key, left, right):
+    self.key = key
+    self.left = left
+    self.right = right
+
 def IsBinarySearchTree(tree):
-  # Implement correct algorithm here
+  nodes = []
+  for node in tree:
+    nodes.append(Node(key=node[0], left= node[1], right= node[2]))
+  stack = [(float('-inf'), nodes[0], float('inf'))]
+  while stack:
+    min_val, root, max_val = stack.pop()
+    if root.key < min_val or root.key >= max_val:
+      return False
+    if root.left != -1:
+      stack.append((min_val, nodes[root.left], root.key))
+    if root.right != -1:
+      stack.append((root.key, nodes[root.right], max_val))
   return True
 
 
@@ -15,7 +34,7 @@ def main():
   tree = []
   for i in range(nodes):
     tree.append(list(map(int, sys.stdin.readline().strip().split())))
-  if IsBinarySearchTree(tree):
+  if nodes == 0 or IsBinarySearchTree(tree):
     print("CORRECT")
   else:
     print("INCORRECT")
